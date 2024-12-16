6359351007ee9fce9bc32d68f91ab290c8983ea1
SPS <sumitpalsingh007@gmail.com>
2024-12-17 03:17:35 +0530

    First Commit

---
+++ src/main/java/com/helloworldconsulting/App.java
@@ -0,0 +1,18 @@
+package com.helloworldconsulting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static final String MAIL = "";
+    public static final String PASSWORD = "";
+    public static final String POST = "https://www.youtube.com/watch?v=GagctuPfJcg";
+    public static final String TITLE = "                                                   " +
+                                       "                           Why was Default Method introduced in Java Interfaces | And how's it different from Abstract Classes";
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/helloworldconsulting/Test.java
@@ -0,0 +1,23 @@
+package com.helloworldconsulting;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Test {
+    public static void main(String[] args) {
+        // Setup ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://www.google.com");
+
+        // Your test code here
+
+        driver.quit();
+    }
+}
